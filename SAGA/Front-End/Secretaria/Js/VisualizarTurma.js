1ccc9c85b6148a2930482ed4fda1caaccef9ee81
VelocityBM <brennomellomudo@gmail.com>
2025-05-21 20:43:47 -0300

    small fixes

--- SAGA/Front-End/Secretaria/Js/VisualizarTurma.js
+++ SAGA/Front-End/Secretaria/Js/VisualizarTurma.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function () {
     const tableBody = document.getElementById("ListaUsuariosTableBody");
 
     const data = [
