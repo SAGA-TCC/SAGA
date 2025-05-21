d247b5632e0ca68a6c560cb6bbdd7c80355c315c
VelocityBM <brennomellomudo@gmail.com>
2025-05-20 22:37:19 -0300

    unfinished search bar and few fixes

--- SAGA/Front-End/Secretaria/Js/ListarCursos.js
+++ SAGA/Front-End/Secretaria/Js/ListarCursos.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const tableBody = document.getElementById("ListaCursosTableBody");
 
     const data = [
-        { codigo: "0", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025"},
-        { codigo: "1", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025"},
-        { codigo: "2", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025"},
-        { codigo: "3", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025"},
-        { codigo: "4", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025"},
-        { codigo: "5", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025"}
+        { codigo: "0", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025" },
+        { codigo: "1", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025" },
+        { codigo: "2", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025" },
+        { codigo: "3", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025" },
+        { codigo: "4", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025" },
+        { codigo: "5", nomeCurso: "Técnico em Informática", dt_cadas: "01/01/2025" }
     ];
 
     data.forEach(item => {
@@ -23,3 +23,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// função de pesquisa
+function searchFunction() {
+    var input = document.getElementById("listInput");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("listTable");
+    var trs = table.tBodies[0].getElementsByTagName("tr");
+
+    // Loop through rows
+    for (var i = 0; i < trs.length; i++) {
+        var tds = trs[i].getElementsByTagName("td");
+        trs[i].style.display = "none";
+
+        for (var j = 0; j < 2; j++) { // verifica as primeiras duas colunas 
+            if (tds[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                trs[i].style.display = ""; // mostra as colunas se forem encontradas
+                break; 
+            }
+        }
+    }
+}
